875403b947882f65d74dddd5ca794e97d70925af
JANANI <vjanani1120@gmail.com>
2025-10-28 17:05:17 +0530

    Data type task uploaded

---
+++ Data_type.py
@@ -0,0 +1,22 @@
+Name="Janani"
+Age=23
+Height=164.50
+Weight=55
+City="Sivagangai"
+is_student=True
+Marks=8.5
+pi=3.14
+School_name="St.Joseph's Matriculation Higher Secondary School"
+My_number=20
+My_text="Hi Everyone! This is Jan"
+print("My name is:", Name)
+print("My age is:", Age)
+print("My Height is:",Height)
+print("My Weight is:",Weight)
+print("My City is:",City)
+print("My mark in CGPA:",Marks)
+print("The value of pi:",pi)
+print("Is she/he student:",is_student)
+print("My School name is:",School_name)
+print("My number:",My_number,",type",type(My_number))
+print("My text:",My_text,",type:",type(My_text))
